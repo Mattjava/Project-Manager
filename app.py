ef5ec3c14151a7289ba0f11d6e8dff8fdd4d0b3d
Matthew Fortes <77417435+Mattjava@users.noreply.github.com>
2025-07-27 11:21:49 -0400

    Revamped the project
    The project now features multiple improvements, such as a Postresql database, a new front-end UI built with CSS and Bootstrap, and many quality-of-life changes.

--- app.py
+++ app.py
@@ -1,64 +1,81 @@
 from flask import Flask, render_template, redirect, request
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from sqlalchemy import Table, Column, MetaData, Sequence, Identity, String
+from sqlalchemy.sql import text
+import os
+
+from wtforms.validators import DataRequired
 
 currentId = 0
-tasks = []
+
+meta = MetaData()
+
+
 
 app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI']
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(255))
+
+    def __init__(self, task):
+        self.task = task
+
+class TaskForm(FlaskForm):
+    task = StringField('Task', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 @app.route('/')
 def index():  # put application's code here
     global currentId, tasks
 
-    connection = sqlite3.connect("taskdatabase.db")
-    cursor = connection.cursor()
 
-    tasks = cursor.execute("SELECT task FROM tasks").fetchall()
+    tasks = db.session.execute(text('SELECT task FROM task')).fetchall()
 
-    allIds = cursor.execute("SELECT id FROM tasks").fetchall()
-    currentId = allIds[len(allIds) - 1][0]
+    allIds = db.session.execute(text('SELECT id FROM task')).fetchall()
 
-    connection.close()
+    tasks_dict = []
+    for i in range(len(tasks)):
+        task_dict = {}
+        task_dict['id'] = allIds[i][0]
+        task_dict['task'] = tasks[i][0]
+        tasks_dict.append(task_dict)
 
-    tasks = {task[0] for task in tasks}
-
-    print(str(tasks) + "| Highest Id: " + str(currentId))
-
-    return render_template('index.html', tasks=tasks)
+    return render_template('index.html', task_dict=tasks_dict)
 
 
 @app.route('/add')
 def add():
-    return render_template('add.html')
-
-@app.route('/delete')
-def delete():
-    return render_template('delete.html')
+    return render_template('add.html', form=TaskForm())
 
 @app.route('/postadd', methods=['POST'])
 def postadd():
+
     if request.method == 'POST':
         task = request.form['task']
+        tasks = db.session.execute(text('SELECT task FROM task')).fetchall()
         if task not in tasks:
             print("Adding new task")
-            connection = sqlite3.connect("taskdatabase.db")
-            cursor = connection.cursor()
-            new_id = currentId + 1
-            cursor.execute("INSERT INTO tasks VALUES(?, ?)", (new_id, task))
-            connection.commit()
-            connection.close()
+
+            new_task = Task(task)
+            db.session.add(new_task)
+            db.session.commit()
     return redirect('/')
 
-@app.route('/postdelete', methods=['POST'])
-def postdelete():
-    if request.method == 'POST':
-        removed_task = request.form.get('task')
-        if removed_task in tasks:
-            connection = sqlite3.connect("taskdatabase.db")
-            cursor = connection.cursor()
-            cursor.execute("DELETE FROM tasks WHERE task = ?", (removed_task,))
-            connection.commit()
-            connection.close()
+@app.route('/postdelete/<int:id>', methods=['GET', 'POST'])
+def postdelete(id):
+    db.session.execute(text('DELETE FROM task WHERE id=:oldid'), {"oldid": id})
+    db.session.commit()
 
     return redirect('/')
 
